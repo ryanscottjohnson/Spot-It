7364ba3c1add197ea408b0d671f5a47e045855ba
ryanscottjohnson <teacherman1@gmail.com>
2018-07-19 17:46:49 -0700

    started app in components folder

---
+++ src/components/app.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Provider} from 'react-redux';
+import {createStore, combineReducers} from 'redux';
+
+import reducers from '../reducers';
+
+const store = createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+import Dashboard from './dashboard.jsx';
+
+class App extends React.Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <Dashboard />
+            </Provider>
+        )
+    }
+}
+
+export default App;
